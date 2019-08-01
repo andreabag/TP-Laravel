9500d6538f1a1fe1fb1ac53a41921037193cac7c
Andrea Bagdadi <andreabagdadi@gmail.com>
2019-07-31 22:03:20 -0300

    migraciones

---
+++ database/migrations/2019_07_31_211352_create_articulos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('foto');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
