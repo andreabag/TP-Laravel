37cdf63a303b15a11cefaaa18af5be68bdc76917
Andrea Bagdadi <andreabagdadi@gmail.com>
2019-08-02 19:07:23 -0300

    registro,seeder,faker

---
+++ database/factories/UsuariosFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+      "name" => $faker->sentence(6),
+      "surname" => $faker->sentence(6),
+      "user" => $faker->sentence(8),
+      "birthdate" => $faker->date(),
+      "avatar" => $faker->sentence(20),
+      "province" => $faker->sentence(6),
+      "password" => $faker->sentence(10),
+      "email" => $faker->sentence(15)
+    ];
+});
