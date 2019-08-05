13235455ad60d214ee8d8f09f79c9dad8d40bdfc
gaston <gaston.estevez7@gmail.com>
2019-08-04 21:32:53 -0300

    arreglo migraciones, funciona todo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar');
+            $table->string('province');
+            $table->date('birthdate');
+            $table->string('user');
+            $table->string('surname');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
